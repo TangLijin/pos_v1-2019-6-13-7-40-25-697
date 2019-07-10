9a5dfcc46418907e68faab81f7513f9ce79d061e
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-10 21:05:00 +0800

    finish all function,but it can not run and I can find the reason

--- main/main.js
+++ main/main.js
@@ -54,16 +54,15 @@ function loadAllItems() {
       }
     ];
   }
-  
-//const fixtures = require("../test/fixtures.js");
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+
+//const fixtures = require("../test/fixtures.js");
 
 let allItems = loadAllItems();
 //console.log(allItems);
 let promotions = loadPromotions();
 
-const getAllItemBarcode = (allItems) =>{
+const getAllItemBarcode = () =>{
     let allItemBarcode = [];
     for(let i = 0; i < allItems.length; i++){
         allItemBarcode.push(allItems[i].barcode);
@@ -74,11 +73,117 @@ const getAllItemBarcode = (allItems) =>{
 const isValid = (allItemBarcode,shopList) => {
     for(let i = 0; i < shopList.length; i++){
         if(allItemBarcode.indexOf(shopList[i]) == -1)
+            console.log("false");
             return false;
     }
+    console.log("true");
     return true;
 }
 
+const getShopListCount = (shopList) => {
+    let shopListCount = [];
+    shopList.sort();
+    var temp = {};
+    for(let i = 0; i < shopList.length - 1; i++) {
+        if(i == 0){
+            temp.barcode = shopList[i];
+            temp.count = 1;
+            if(shopList[0] === shopList[1])
+                ++ temp.count;
+            continue;
+        }
+        if(shopList[i] === shopList[i+1]){
+            ++temp.count;
+        }else{
+            shopListCount.push(temp);
+            temp = {};
+            temp.barcode = shopList[i+1];
+            temp.count = 1;
+        }
+    }
+    if(temp.length != 0)
+        shopListCount.push(temp);
+    return shopListCount;
+}
+
+const getItemCart = (shopListCount) => {
+    let itemCart = [];
+    
+    for(let i = 0; i < shopListCount.length; i++){
+        let item = {};
+        for(let j = 0; j < allItems.length; j++){
+            if(shopListCount[i] == allItems[j].barcode){
+                item.barcode = shopList[i];
+                item.name = allItems[j].name;
+                item.unit = allItems[j].unit;
+                item.price = allItems[j].price;
+                item.count = shopListCount[i];
+                item.sumPrice = shopListCount[i] * allItems[j].price;
+                /*item.barcode = shopList[i];
+                item.名称 = allItems[j].name;
+                item.数量 = 1;
+                item.名称 = allItems[j].name;*/
+            }
+        }
+        itemCart.push(item);
+    }
+    return itemCart;
+}
+
+const getItemCartPromotion = (itemCart) => {
+    let itemCartPromotion = [];
+    if(promotions.length == 0){
+        return itemCart;
+    }
+    let promotionbarcodes = promotions[0].barcodes;
+    var item = {}
+    for(let i = 0; i < itemCart.length; i++){
+        item.barcode = itemCart[i].barcode;
+        item.name = itemCart[i].name;
+        item.unit = itemCart[i].unit;
+        item.price = itemCart[i].price;
+        item.count = itemCart[i].count;
+        item.sumPrice = itemCart[i].sumPrice;
+        if(promotionbarcodes.indexOf(itemCart[i].barcode) != -1 ){
+            if(item.count >= 2){
+                item.sumPrice -= item.price
+            }
+        }
+        itemCartPromotion.push(item);
+    }
+    return itemCartPromotion;
+}
 //console.log(getAllItemBarcode(allItems));
+
+const createReceipt = (itemCartPromotion) => {
+    let receipt = `***<没钱赚商店>收据***`;
+    let discount = 0;
+    let sumMoney = 0;
+    for(let i = 0; i < itemCartPromotion.length; i++){
+        receipt += `名称：${itemCartPromotion[i].name}，数量:${itemCartPromotion[i].count}${itemCartPromotion[i].unit}，
+        单价:${itemCartPromotion[i].price}（元），小计：${itemCartPromotion[i].sumPrice}（元）`;
+        discount += (itemCartPromotion[i].count * itemCartPromotion[i].price - itemCartPromotion[i].sumPrice);
+        sumMoney += itemCartPromotion[i].sumPrice;
+    }
+    receipt +=`
+---------------------
+总计：${sumMoney}(元)
+节省：${discount}(元)
+**********************
+`
+    return  receipt;
+}
+
+const printReceipt = (shopList) =>{
+    let allItemBarcode = getAllItemBarcode();
+    if(!isValid(allItemBarcode))
+        return "[ERROR]:no this barcode";
+    let shopListCount = getShopListCount(shopList);
+    let itemCart = getItemCart(shopListCount);
+    let itemCartPromotion = getItemCartPromotion(itemCart);
+    let receipt = createReceipt(itemCartPromotion);
+    console.log(receipt);
+    return createReceipt;
+}
 
 module.exports = {getAllItemBarcode};
